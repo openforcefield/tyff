412338a46b06a39ae66d8e150d362c555018fc2d
Matthew W. Thompson <mattwthompson@protonmail.com>
2026-09-24 13:29:42 -0500

    Enable updating force constants when `k` is list-like

--- tyff/_tests/convertors/openff/test_tensors.py
+++ tyff/_tests/convertors/openff/test_tensors.py
@@ -9,6 +9,19 @@ from openff.toolkit.typing.engines.smirnoff.parameters import VirtualSiteType
 import tyff.converters
 
 
+def tidy_force_field(force_field: ForceField) -> ForceField:
+    # work around vdWType squishiness when comparing force fields via hash
+    # https://github.com/openforcefield/openff-toolkit/issues/2241
+    for parameter in force_field["vdW"].parameters:
+        parameter.rmin_half = round(parameter.rmin_half, 12)
+
+    return force_field
+
+
+def force_fields_are_equal(force_field_1: ForceField, force_field_2: ForceField) -> bool:
+    return hash(tidy_force_field(force_field_1)) == hash(tidy_force_field(force_field_2))
+
+
 @pytest.fixture
 def sage():
     return ForceField("openff-2.3.0.offxml")
@@ -42,6 +55,12 @@ def methyl_chloride():
     return Molecule.from_smiles("CCl")
 
 
+@pytest.fixture
+def methyl_phenyl_disulfide():
+    # need a molecule with layered torsions and impropers
+    return Molecule.from_smiles("c1ccccc1(SSC)")
+
+
 def test_convert_no_modifications(phenol, sage):
     """
     Test basic behavior of convert_tensor_force_field with no modifications of inputs.
@@ -55,7 +74,15 @@ def test_convert_no_modifications(phenol, sage):
         tensor_force_field,
     )
 
-    assert hash(new_force_field) == hash(sage)
+    try:
+        force_fields_are_equal(new_force_field, sage)
+    except AssertionError as error:
+        import copy
+
+        new_force_field.to_file("new.offxml")
+        copy.deepcopy(sage).to_file("old.offxml")
+
+        raise error
 
 
 @pytest.mark.parametrize(
@@ -67,14 +94,14 @@ def test_convert_no_modifications(phenol, sage):
         ("ImproperTorsions", 0),  # k, etc.
     ],
 )
-def test_convert_after_perturbation(phenol, sage, handler_to_perturb, column_index):
+def test_convert_after_perturbation(methyl_phenyl_disulfide, sage, handler_to_perturb, column_index):
     """
     Test that a tensor force field, randomly perturbed from the original SMIRNOFF source parameters, can be
     converted back into a SMIRNOFF force field.
     """
     factor = random.random()
 
-    interchange = sage.create_interchange(phenol.to_topology())
+    interchange = sage.create_interchange(methyl_phenyl_disulfide.to_topology())
 
     tensor_force_field, _ = tyff.converters.convert_interchange(interchange)
 
@@ -110,6 +137,27 @@ def test_convert_after_perturbation(phenol, sage, handler_to_perturb, column_ind
     for value in found_factors:
         assert value == pytest.approx(factor), (
             f"Expected k to be scaled by {factor}, but found to be scaled by {value}"
+        )
+
+
+def test_convert_vsites_unsupported(methyl_chloride, sage_with_bond_charge):
+    """
+    Test that an error is raised when attempting to convert back a tensor force field
+    which contatins virtual site parameters.
+
+    Remove this test when the feature is implemented.
+    """
+
+    interchange = sage_with_bond_charge.create_interchange(methyl_chloride.to_topology())
+
+    tensor_force_field, _ = tyff.converters.convert_interchange(interchange)
+
+    # modify tensor force field
+
+    with pytest.raises(NotImplementedError):
+        tyff.converters.convert_tensor_force_field(
+            sage_with_bond_charge,
+            tensor_force_field,
         )
 
 
--- tyff/converters/openff/_tensors.py
+++ tyff/converters/openff/_tensors.py
@@ -1,5 +1,6 @@
 import openff.interchange
 import openff.toolkit
+from openff.toolkit.utils.exceptions import ParameterLookupError
 
 import tyff
 
@@ -10,25 +11,44 @@ def convert_tensor_force_field(
 ) -> openff.toolkit.ForceField:
     """
     Convert a tensor force field back into a SMIRNOFF force field, using the
-    original force field as a template. Electrostatics / applied partial
-    charges are not updated.
+    original force field as a template.
+
+    Electrostatics / applied partial charges are not updated.
+
+    `ImproperTorsionType.idivf` values are ignored.
+
+    Virtual site parameters are not (yet) supported.
     """
     import copy
 
     updated = copy.deepcopy(original_force_field)
 
     for potential in tensor_force_field.potentials:
-        if potential.type in ("Electrostatics", "ProperTorsions", "ImproperTorsions"):
+        if potential.type in ("Electrostatics"):
             continue
+
+        if potential.type in ("VirtualSites"):
+            raise NotImplementedError("Virtual site parameters are not yet supported.")
 
         name = str(getattr(potential.type, "value", potential.type))
         handler = updated.get_parameter_handler(name)
 
         for row, key in enumerate(potential.parameter_keys):
             for index, col in enumerate(potential.parameter_cols):
+                if col == "idivf":
+                    continue
+
                 value = potential.parameters[row, index].item()
-                # skip if parameter is already set to this value?
-                # maybe unnecessarily slow ...
-                setattr(handler[key.id], col, value * potential.parameter_units[index])
+                try:
+                    # skip if parameter is already set to this value?
+                    # maybe unnecessarily slow ...
+                    setattr(handler[key.id], col, value * potential.parameter_units[index])
+                except TypeError:
+                    # can't set a list of k with just one k value, but can directly set the element
+                    # of this parameter's k-list
+                    getattr(handler[key.id], col)[key.mult] = value * potential.parameter_units[index]
+                except ParameterLookupError as error:
+                    # this is probably a failure to look up a virtual site's vdW parameter
+                    raise NotImplementedError("Virtual site parameters are not yet supported.") from error
 
     return updated
